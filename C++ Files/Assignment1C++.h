35db651da2348b57edfa036fedbea5367fd25b90
Michael Waterworth <mike@3equals.co.uk>
2014-11-17 13:28:48 +0000

    Revert to working
    Not working is
    	Printing

--- C++ Files/Assignment1C++.h
+++ C++ Files/Assignment1C++.h
@@ -10,45 +10,37 @@ using namespace std;
 
 /***
 *  update: computes new values for timestep t+delta_t
+ nx and ny are distribution values for x and y dimensions - limits propogation
 ***/
-void update(int nx, int ny, double u1[][100], double u2[][100])
+void update(double nx, double ny, double u1[][100], double u2[][100])
 {
     int ix, iy;
+
 	/* copy boundary conditions from old_u to new_u */
     for (ix = 0; ix <= XDIM-1; ix++) //Top and bottom rows
     {
-    	u1[nx-1][ix] = u2[nx-1][ix];
+    	u1[XDIM-1][ix] = u2[XDIM-1][ix];
     	u1[0][ix] = u2[0][ix];
-        // TO BE COMPLETED
     }
     for (iy = 0; iy <= YDIM-1; iy++) //Down both edges
     {
     	u1[iy][0] = u2[iy][0];
-    	u1[iy][nx-1] = u2[iy][nx-1];
-        // TO BE COMPLETED 
+    	u1[iy][YDIM-1] = u2[iy][YDIM-1];
     }
 
- 	
- 	//cout << u1 << endl;
-
- 	for(ix = 1;ix < nx - 1; ++ix)
+ 	for(ix = 1;ix < XDIM - 1; ++ix)
 	{
-		for(iy = 1;iy < ny - 1; ++iy)
+		for(iy = 1;iy < YDIM - 1; ++iy)
 		{
-			//Test using Cx & Cy = 0.1 - Needs to come from command line
 			u1[ix][iy] = // Ux,y,t
 			u2[ix][iy] // Ux,y,t−1
 			+ nx * // + Cx *
-				(u2[ix+1][iy] + u2[ix-1][iy] - 2 * u2[ix][iy])
+				(u2[ix+1][iy] + u2[ix-1][iy] - 2 * u2[ix][iy]) //(Ux+1,y,t−1 + Ux−1,y,t−1 − 2  Ux,y,t−1)
 			+ ny * // + Cy *
 				(u2[ix][iy+1] + // (Ux,y+1,t−1 + 
 				u2[ix][iy-1] - 2 * u2[ix][iy]); // Ux,y−1,t−1 − 2 * Ux,y,t−1)
 		}
 	}
-
- 	//Ux,y,t = Ux,y,t−1 + Cx * (Ux+1,y,t−1 + Ux−1,y,t−1 − 2  Ux,y,t−1) + Cy * (Ux,y+1,t−1 + Ux,y−1,t−1 − 2 * Ux,y,t−1)
-        /* TO BE COMPLETED */
- 
 }
  
 /***
@@ -56,27 +48,22 @@ void update(int nx, int ny, double u1[][100], double u2[][100])
 ***/
 void initdata(double u1[XDIM][YDIM], char* fname)
 {
-/* TODO*/
-
 	int ix, iy;
-	printf("\n\n\n");
 	ifstream fp(fname);
 	
-	if (fp == NULL)
+	if (!fp)
 	{
 	  fprintf(stderr, "Can't open input file %s!\n",fname);
 	  exit(1);
 	}
-
-
 	
-	for(ix=0;ix<YDIM;ix++)
+	for(ix=0;ix<XDIM;ix++)
 	{
 		string line;
 		getline(fp,line);
 		stringstream src (line);
 		
-		for(iy=0;iy<XDIM;iy++)
+		for(iy=0;iy<YDIM;iy++)
 		{
 			string val;
 			int temp=0;
@@ -85,13 +72,8 @@ void initdata(double u1[XDIM][YDIM], char* fname)
 			end.clear();
 			end.str("");
 			end.str(val);
-			end>>u1[ix][iy];
-			//u1[ix][iy]=temp;
-			//printf("%d \n",(int)u1[ix][iy]);  //FINALLY GOT THIS WORKING, NOW IM DEAD TIRED !!!! STUPID FLOAT PRECISION
-			//cout<<"ul["<<ix<<"]["<<iy<<"] is "<<u1[ix][iy]<<'\n';
-			
-		}
-		
+			end>>u1[ix][iy];			
+		}		
 	}
 }
 
@@ -99,15 +81,14 @@ void initdata(double u1[XDIM][YDIM], char* fname)
 /***
 *  printdata: generates a .csv file with data contained in parameter double* u1 
 ***/
- 
 void prtdata(int nx, int ny, int ts, double *u1, char* fname)
 {
     int ix, iy;
 	FILE *fp;
 	char* ffname = fname;
 	sprintf(fname,"%s%d.csv",fname,ts);
+	fp = fopen(ffname, "w");
     for (iy = ny-1; iy >= 0; iy--) {
-    fp = fopen(ffname, "w");
         for (ix = 0; ix <= nx-1; ix++) {
             fprintf(fp, "%8.3f,", *(u1+ix*ny+iy));
             if (ix != nx-1) {
--- C++ Files/Assignment1C++File.cpp
+++ C++ Files/Assignment1C++File.cpp
@@ -9,10 +9,6 @@ HPC Assignment 1 C Part */
 #include "Assignment1C++.h"
 //set dimensions of array
 
-
-
-
-
 struct Params
 {
 	double cx,cy;  //grid points
@@ -22,64 +18,43 @@ struct Params
 
 int main(int argc, char *argv[])
 {
-    //Put parameters in to Params Struct
     //./temp_dist2D 0.1 0.1 50 initial_data.csv
-   
+
+    /* Read in grid sampling paameters and timesteps and initialize struct Params */
 	Params para;
 	para.cx=strtod(argv[1],NULL);
 	para.cy=strtod(argv[2], NULL);
 	para.nts=strtod(argv[3], NULL);
 
-	
-	    double old_u[XDIM][YDIM];
+	/* Read grid dimension dim and allocate old_u and new_u */
+	double old_u[XDIM][YDIM];
     double new_u[XDIM][YDIM];
-    //params para = params{argv[1],argv[2],argv[3]};
-    int ix, iy, iz, it, ts; /* iterators */
+    printf("Using [%d][%d] grid.\n", XDIM, YDIM);
+
+    int it = 0; // Ts iterator
     char *fname= argv[4];
     char nname[] = "data_";
     
-    int nts = 100;
-    it = 0;
-
-
     printf("Starting serial version of 2D temperature distribution example...\n");
  
-    /* Read in grid dimension dim and allocate old_u and new_u */
-	
-    printf("Using [%d][%d] grid.\n", XDIM, YDIM);
-	
-    /* Read in grid sampling paameters and timesteps and initialize struct Params */
- 
- 
     /* Initialize grid from input file */
-    printf("Initializing grid from input file: %s",fname);
- 
+    printf("Initializing grid from input file: %s\n",fname);
     initdata(old_u, fname);
 
-
-	//copy initialized grid to new grid,
-    for(ix=0;ix<XDIM;++ix)
-    {
-        for(iy=0;iy<YDIM;++iy)
-        {
-           new_u[ix][iy]= old_u[ix][iy];
-			//cout << old_u[ix][iy]<<'\n';
-        }
-    }
- 
-    // Iterate over all timesteps and create final output file (e.g. timestep = nts) 
+    // Iterate over all timesteps and create output files
     printf("Iterating over %d time steps...\n", para.nts);
     while(it < para.nts){
-        if(it % 2 == 1){//if timestep is even 
+        cout << it << " " << para.nts << endl;
+        if(it % 2 == 1){//if timestep is odd 
             update(para.cx,para.cy, old_u, new_u);
-        } else {// if timestep is odd 
+        } else {// if timestep is even 
             update(para.cx,para.cy, new_u, old_u);
         }
         it++;
     }
-    printf("Done. Created output file: %d\n", it);
 
-    prtdata(XDIM, YDIM, it, *new_u, nname);
+    prtdata(XDIM, YDIM, it, *old_u, nname);
+    printf("Done. Created output file: %d\n", it);
 
 	getchar();
 	return 0;
